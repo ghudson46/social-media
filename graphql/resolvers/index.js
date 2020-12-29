20d07175e686525872633c984a2093528a46baa3
Garrett Hudson <garretthudson87@yahoo.com>
2020-12-29 14:13:13 -0500

    created createComment resolver

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const PostResolvers = require('./posts');
 const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
 
 module.exports = {
     Query: {
@@ -7,6 +8,7 @@ module.exports = {
     },
     Mutation: {
         ...usersResolvers.Mutation,
-        ...PostResolvers.Mutation
+        ...PostResolvers.Mutation,
+        ...commentsResolvers.Mutation
     }
 };
