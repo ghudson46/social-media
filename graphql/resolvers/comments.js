5c6b80f9a3022ff64a69f5e20755af45d80f460d
Garrett Hudson <garretthudson87@yahoo.com>
2020-12-29 14:28:22 -0500

    deleting comments resolver is working

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,9 +1,11 @@
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 
+const checkAuth = require('../../util/check-auth');
 const Post = require('../../models/Post');
 
 module.exports = {
     Mutation: {
+        // Creates a comment and saves it to the database
         async createComment(_, { postId, body }, context) {
             const { username } = checkAuth(context);
             if (body.trim() === '') {
@@ -23,6 +25,26 @@ module.exports = {
                 })
                 await post.save();
                 return post;
+            } else {
+                throw new UserInputError('Post not found');
+            }
+        },
+        // Deletes a comment from the database (by ID)
+        async deleteComment(_, { postId, commentId}, context) {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if (post) {
+                const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+                if (post.comments[commentIndex].username === username) {
+                    post.comments.splice(commentIndex, 1);
+                    await post.save();
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                }
             } else {
                 throw new UserInputError('Post not found');
             }
