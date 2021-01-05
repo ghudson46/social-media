274a22955b56c8feceef3539da3d654aef13dfd4
Garrett Hudson <garretthudson87@yahoo.com>
2021-01-05 11:31:31 -0500

    added nav bar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NavBar from './components/NavBar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>19th hole</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
   );
 }
 
