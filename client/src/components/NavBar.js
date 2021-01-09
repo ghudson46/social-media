4ae4a837f824cf2986420e5aae750c3681b84144
Garrett Hudson <garretthudson87@yahoo.com>
2021-01-09 12:23:59 -0500

    got navbar links to user react-router-dom links to link between pages and added some styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,28 +1,35 @@
-import React, { useState } from 'react'
-import { Menu } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
-  const [activeItem, setActiveItem] = useState('');
+  const [activeItem, setActiveItem] = useState('home');
 
   const handleItemClick = (e, { name }) => setActiveItem(name)
 
     return (
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color="green">
             <Menu.Item
             name='home'
             active={activeItem === 'home'}
             onClick={handleItemClick}
+            as={Link}
+            to="/"
             />
           <Menu.Menu position='right'>
             <Menu.Item
                 name='login'
                 active={activeItem === 'login'}
                 onClick={handleItemClick}
+                as={Link}
+                to="/login"
             />
             <Menu.Item
                 name='register'
                 active={activeItem === 'register'}
                 onClick={handleItemClick}
+                as={Link}
+                to="/register"
             />
           </Menu.Menu>  
         </Menu>
