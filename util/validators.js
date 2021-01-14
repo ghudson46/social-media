223a493e8e518999795014f9bddefe4fdd27a433
Garrett <garrett@growers.ag>
2021-01-14 00:16:47 -0500

    completed register page. getting a regex email error

--- util/validators.js
+++ util/validators.js
@@ -15,14 +15,14 @@ class Validators {
         if (!username || !username.trim()) {
             errors.username = 'Username must not be empty';
         }
-        if (email.trim() === '') {
+        if (!email || !email.trim()) {
             errors.email = 'Email must not be empty';
-          } else {
-            const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-            if (!email.match(regEx)) {
-              errors.email = 'Email must be a valid email address';
+        } else {
+            // test() returns a boolean
+            if (!EMAIL_REGEX.test(email)) {
+                errors.email = 'Email must be a valid email address';
             }
-          }
+        }
         if (!password) {
             errors.password = 'Password must not be empty';
         } else if (password !== confirmPassword) {
